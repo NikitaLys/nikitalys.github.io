1d945b9b6f5f85ae03774e69b49eb8be30df00c6
Nikita Lysenko <nikita.lysenko1@gmail.com>
2022-03-11 10:53:35 +0200

    new site in projekt

--- README.md
+++
@@ -1,2 +0,0 @@
-# Blanchard
-Graduate work(basic)
--- js/header__burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".header-top__burger-opn").addEventListener("click", function() {
-        document.querySelector(".header-top__menu").classList.add("active");
-    })
-    document.querySelector(".header-top__burger-close").addEventListener("click", function() {
-        document.querySelector(".header-top__menu").classList.remove("active");
-    })
-})
